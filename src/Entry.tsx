44e34a6ec894ed695c93ee5fc14b85b06ac6cd12
mattboan <matt.n.boan@gmail.com>
2023-01-10 16:57:53 +0800

    first commit

---
+++ src/Entry.tsx
@@ -0,0 +1,9 @@
+// This is where the application enters
+export const Entry = () => {
+    return (
+        <div className="entry-point">
+            <h1>Entry Point</h1>
+            <p>Here is where the application enters</p>
+        </div>
+    );
+};
